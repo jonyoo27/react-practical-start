a6d13ad46cf2677e52eff6f58df09a6d0059fe85
Steve Yoo <sungtae.yoo@costco.com>
2024-09-30 12:54:14 -0700

    problem ready

--- 05/demos/src/house/index.js
+++ 05/demos/src/house/index.js
@@ -11,25 +11,17 @@ const House = ({ house }) => {
   return (
     <div>
       <div className="row mt-2">
-        <h5 className="col-md-12">{house.country}</h5>
+        <h5 className="col-md-12"></h5>
       </div>
       <div className="row">
-        <h3 className="col-md-12">{house.address}</h3>
+        <h3 className="col-md-12"></h3>
       </div>
       <div className="row">
         <div className="col-md-7">
-          <img src={`/images/${house.photo}.jpeg`} alt="House" />
         </div>
         <div className="col-md-5">
-          <p className="price">${house.price}</p>
-          <p>{house.description}</p>
-          <img
-            src={emailIcon}
-            height="50"
-            alt="inquiry"
-            onClick={inquiryClick}
-          />
-          {inquiryShown && <Inquiry house={house} />}
+
+
         </div>
       </div>
     </div>

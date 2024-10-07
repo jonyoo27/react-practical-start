5b0300b7189b2598d233438038dcfb452da40f47
Steve Yoo <sungtae.yoo@costco.com>
2024-10-07 16:00:32 -0700

    Problem Ready

--- 05/demos/src/main-page/house-filter.js
+++ 05/demos/src/main-page/house-filter.js
@@ -1,16 +1,14 @@
 import { useHistory } from "react-router-dom";
 
 const HouseFilter = ({ allHouses }) => {
-  const history = useHistory();
+  //const history;
 
-  const countries = allHouses
-    ? Array.from(new Set(allHouses.map((h) => h.country)))
-    : [];
+  const countries = [];
+  
   countries.unshift(null);
 
   const onSearchChange = (e) => {
-    const country = e.target.value;
-    history.push(`/searchresults/${country}`);
+
   };
 
   return (
@@ -19,12 +17,8 @@ const HouseFilter = ({ allHouses }) => {
         Look for your dream house in country:
       </div>
       <div className="col-md-4 mb-3">
-        <select className="form-select" onChange={onSearchChange}>
-          {countries.map((c) => (
-            <option key={c} value={c}>
-              {c}
-            </option>
-          ))}
+        <select className="form-select" >
+
         </select>
       </div>
     </div>

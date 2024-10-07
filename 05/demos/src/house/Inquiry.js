aacce0610b52bd68ce8644fda02cbb06f5811f97
Steve Yoo <sungtae.yoo@costco.com>
2024-10-07 15:00:43 -0700

    problem ready

--- 05/demos/src/house/Inquiry.js
+++ 05/demos/src/house/Inquiry.js
@@ -8,7 +8,7 @@ const Inquiry = () => {
   });
 
   const onChange = (e) => {
-    setContactInfo({ ...contactInfo, [e.target.id]: e.target.value });
+
   };
 
   const onSubmit = (e) => {
@@ -21,40 +21,28 @@ const Inquiry = () => {
       <div className="form-group">
         <label htmlFor="name">Name</label>
         <input
-          type="text"
-          className="form-control"
-          placeholder="Name"
-          id="name"
-          value={contactInfo.name}
-          onChange={onChange}
+          type=""
+          className=""
+          placeholder=""
+          id=""
+
         />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email address</label>
         <input
-          type="text"
-          id="email"
-          className="form-control"
-          placeholder="Email"
-          value={contactInfo.email}
-          onChange={onChange}
+
         />
       </div>
       <div className="form-group">
         <label htmlFor="remarks">Remarks</label>
         <input
-          type="text"
-          id="remarks"
-          className="form-control"
-          placeholder="Remarks"
-          value={contactInfo.remarks}
-          onChange={onChange}
+
         />
       </div>
       <button
         className="btn btn-primary mt-2"
-        disabled={!contactInfo.name || !contactInfo.email}
-        onClick={onSubmit}
+
       >
         Submit
       </button>
